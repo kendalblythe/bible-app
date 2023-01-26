4e41cda8864218dd308ab2f0d397eaf714767480
Kendal Blythe <kendal.blythe@gmail.com>
2023-01-26 17:57:17 -0500

    show copyright

--- apps/bible-nextjs-app/src/views/BooksView.tsx
+++ apps/bible-nextjs-app/src/views/BooksView.tsx
@@ -1,3 +1,8 @@
+import ReactMarkdown from 'react-markdown';
+
+import rehypeRaw from 'rehype-raw';
+import remarkGfm from 'remark-gfm';
+
 import { useBibleQuery, useBooksQuery } from '../api/queries';
 import {
   ButtonListItem,
@@ -22,25 +27,52 @@ export const BooksView = () => {
 
   return (
     <>
-      <PageHeader>
-        <div className="flex-1">
-          <PageHeading onBackClick={() => setBibleId(undefined)}>
-            {t('BooksView.page.title', {
-              bible: bible?.abbreviationLocal,
-            })}
-          </PageHeading>
-        </div>
-      </PageHeader>
+      {bible ? (
+        <>
+          <PageHeader>
+            <div className="flex-1">
+              <PageHeading onBackClick={() => setBibleId(undefined)}>
+                {t('BooksView.page.title')}
+              </PageHeading>
+            </div>
+            <div className="flex-none gap-2 ml-4">
+              <span className="badge badge-lg hidden sm:inline-flex">{bible.nameLocal}</span>
+              <span className="badge badge-lg inline-flex sm:hidden">
+                {bible.abbreviationLocal}
+              </span>
+            </div>
+          </PageHeader>
 
-      <PageMain>
-        <List>
-          {books?.map((book) => (
-            <ButtonListItem key={book.id} className="btn-sm" onClick={() => setBookId(book.id)}>
-              {book.name}
-            </ButtonListItem>
-          ))}
-        </List>
-      </PageMain>
+          <PageMain>
+            <List>
+              {books?.map((book) => (
+                <ButtonListItem key={book.id} className="btn-sm" onClick={() => setBookId(book.id)}>
+                  {book.name}
+                </ButtonListItem>
+              ))}
+            </List>
+            <hr className="m-4" />
+            <div className="mx-8 text-center markdown text-sm">
+              <div className="font-medium mt-4 mb-1">About</div>
+              <div>
+                <ReactMarkdown rehypePlugins={[rehypeRaw]} linkTarget="_blank">
+                  {bible.info}
+                </ReactMarkdown>
+              </div>
+              <div className="font-medium mt-4 mb-1">Copyright</div>
+              <div>
+                <ReactMarkdown
+                  remarkPlugins={[remarkGfm]}
+                  rehypePlugins={[rehypeRaw]}
+                  linkTarget="_blank"
+                >
+                  {bible.copyright}
+                </ReactMarkdown>
+              </div>
+            </div>
+          </PageMain>
+        </>
+      ) : null}
 
       {isLoading ? <PageSpinner /> : null}
     </>
