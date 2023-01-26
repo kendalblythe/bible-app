3452f1b66fff7960f4c91ddde18b6afe731c47c1
Kendal Blythe <kendal.blythe@gmail.com>
2023-01-25 23:41:32 -0500

    export default views

--- apps/bible-nextjs-app/src/views/BiblesView.tsx
+++ apps/bible-nextjs-app/src/views/BiblesView.tsx
@@ -67,3 +67,5 @@ export const BiblesView = () => {
     </>
   );
 };
+
+export default BiblesView;
--- apps/bible-nextjs-app/src/views/BooksView.tsx
+++ apps/bible-nextjs-app/src/views/BooksView.tsx
@@ -50,3 +50,5 @@ export const BooksView = () => {
     </>
   );
 };
+
+export default BooksView;
