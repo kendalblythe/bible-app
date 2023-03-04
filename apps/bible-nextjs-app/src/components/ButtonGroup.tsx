e6d49b4368279cde2a84f130f26a7050534d9357
Kendal Blythe <kendal.blythe@gmail.com>
2023-03-03 21:19:03 -0500

    limit button group width

--- apps/bible-nextjs-app/src/components/ButtonGroup.tsx
+++ apps/bible-nextjs-app/src/components/ButtonGroup.tsx
@@ -17,6 +17,7 @@ export const ButtonGroup = ({ buttons }: ButtonGroupProps) => {
           key={text}
           className={clsx(
             'btn btn-sm text-black text-base font-medium',
+            'inline-block max-w-[10rem] truncate normal-case px-2',
             'bg-gray-200 border-gray-200 hover:bg-gray-300 hover:border-gray-300',
             i > 0 && 'ml-px'
           )}
